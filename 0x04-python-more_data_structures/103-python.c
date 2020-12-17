327e3d4340ca56b923e23289ab3036fdb982cc8b
mounirchebbi <2157@holbertonschool.com>
2020-12-18 00:49:19 +0100

    empty

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+/**
+*print_python_list- print python lists infos
+*@p : python list object
+*/
+void print_python_list(PyObject *p)
+{
+	printf("");
+}
+/**
+*print_python_bytes - print python bytes infos
+*@p : python byte object
+*/
+void print_python_bytes(PyObject *p)
+{
+	printf("[ERROR] Invalid Bytes Object\n");
+}
