9575c8397caf6690a783398f865623d8e8ba20e1
mounirchebbi <2157@holbertonschool.com>
2021-01-06 00:22:03 +0100

    square 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Square Class"""
+
+    def __init__(self, size):
+        """Initialization
+
+        Args:
+            size (int): Square Size.
+        """
+        self.__size = size
