6dcfc3257da6eea5a28f227dad8fcae34824733a
mounirchebbi <2157@holbertonschool.com>
2021-01-11 23:58:08 +0100

    string represtation of rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -62,3 +62,9 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+
+    def __repr__(self):
+        """Return Rectangle string representation"""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
