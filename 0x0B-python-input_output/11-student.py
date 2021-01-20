3fbf06b7bfdcb32797e03ff1424fb1ce5e6c8e31
mounirchebbi <2157@holbertonschool.com>
2021-01-21 00:46:15 +0100

    fix

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Definition of class Student"""
+
+
+class Student:
+    """A student"""
+
+    def __init__(self, first_name, last_name, age):
+        """New Student
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary representation of student
+
+        Args:
+            attrs (list): attributes
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes
+
+        Args:
+            json (dict): The key/value pairs to replace attributes
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
