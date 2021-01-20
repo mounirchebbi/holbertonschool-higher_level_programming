928abca53dc7b0a2da61ae0fd8f25335bbac29cb
mounirchebbi <2157@holbertonschool.com>
2021-01-21 00:41:12 +0100

    fix

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Definition of class Student"""
+
+
+class Student:
+    """A student"""
+
+    def __init__(self, first_name, last_name, age):
+        """New Student.
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of student"""
+        return self.__dict__
