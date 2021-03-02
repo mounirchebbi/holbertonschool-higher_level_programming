2df9bab8980f71158e6379cca5d910e274f6874b
mounirchebbi <2157@holbertonschool.com>
2021-03-02 23:59:32 +0100

    SQL_more_queries

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Listing all cities of CA from hbtn_0d_usa
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
