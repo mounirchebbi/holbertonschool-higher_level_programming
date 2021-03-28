e0cca9d77b59a886fb8bbebc3b2dec1e7052d049
mounirchebbi <2157@holbertonschool.com>
2021-03-29 00:33:52 +0100

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N
-from the database hbtn_0e_0_usa
+Lists all states with a name starting with N from the database hbtn_0e_0_usa
 """
 
 import sys
