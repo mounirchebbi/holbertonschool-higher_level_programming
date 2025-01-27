f4c6f44685062658800499b69cb6764d5fb8a205
mounirchebbi <2157@holbertonschool.com>
2025-01-27 21:19:50 +0100

    last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print("{}".format(abs(number) % 10, end=""))
+    print("{}".format(abs(number) % 10), end="")
     return (abs(number) % 10)
+
+print_last_digit(-55)
