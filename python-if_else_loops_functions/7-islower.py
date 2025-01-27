f0699996c7c32138427803802920cc2b2e02fb77
mounirchebbi <2157@holbertonschool.com>
2025-01-27 19:43:44 +0100

    islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if(ord(c) >= ord ('a') and ord(c) <= ord('z')):
-        return True
+    if (ord(c) in range(ord('a'), ord('z')+1)):
+        return (True)
     else:
-        return False
+        return (False)
