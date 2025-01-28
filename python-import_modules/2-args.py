e3c90d450adb8ad816389aa812b04a53aa2996d7
mounirchebbi <2157@holbertonschool.com>
2025-01-28 12:57:15 +0100

    sys.argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 # 2-args.py
-
-import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("1 argument:\n1: {}".format(sys.argv[1]))
-else:
-    i = 0
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)-1):
-        print("{}: {}".format(i+1, sys.argv[i+1]))
+if __name__ == "__main__":
+    """print arguments"""
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    else:
+        i = 0
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)-1):
+            print("{}: {}".format(i+1, sys.argv[i+1]))
