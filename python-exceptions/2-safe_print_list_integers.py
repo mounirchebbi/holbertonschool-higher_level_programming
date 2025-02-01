75c4df4f74b3719cf9a0af2aced877183cf651bf
mounirchebbi <2157@holbertonschool.com>
2025-02-01 22:32:03 +0100

    try try except except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,16 +3,18 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
+    printed_counter = 0
     for item in my_list:
         try:
             if counter < x:
                 try:
                     if isinstance(item, int):
                         print("{:d}".format(item), end="")
-                        counter += 1
+                        printed_counter += 1
+                    counter += 1
                 except Exception as e:
                     print("input type error")
         except IndexError as e:
             print("list index out of range")
     print()
-    return counter
+    return printed_counter
