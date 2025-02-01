ecf0541505d1433cb465e8abca90bff27b527bf2
mounirchebbi <2157@holbertonschool.com>
2025-02-01 13:18:23 +0100

    dict.items()

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,8 +4,13 @@
 def complex_delete(a_dictionary, value):
     if a_dictionary is None or value is None:
         return None
+    _keys = []
     for k, v in a_dictionary.items():
         if v is value:
-            a_dictionary.pop(k)
+            _keys.append(k)
+    if len(_keys) == 0:
+        return (a_dictionary)
+    for k in _keys:
+        a_dictionary.pop(k)
 
     return (a_dictionary)
