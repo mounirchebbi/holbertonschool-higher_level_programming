4c581bf51e05d92fc168b0e4a47a0d25ef3e9016
mounirchebbi <2157@holbertonschool.com>
2025-02-01 11:24:48 +0100

    roman_dict

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# 12-roman_to_int.py
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or (roman_string is None):
+        return 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                   'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    previous = 0
+    for c in reversed(roman_string):
+        if c not in roman_dict.keys():
+            return 0
+        value = roman_dict[c]
+        if value >= previous:
+            result += value
+        else:
+            result -= value
+        previous = value
+        value = 0
+    return result
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
