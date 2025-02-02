c1f73f55e5004dee267d9afa3b0267399651258f
mounirchebbi <2157@holbertonschool.com>
2025-02-02 14:12:38 +0100

    file=sys.stderr

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # 100-safe_print_integer_err.py
+import sys
+
 
 def safe_print_integer_err(value):
-    import sys
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except TypeError as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
