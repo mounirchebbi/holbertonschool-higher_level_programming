88fd77e65bca119f7398a10e724f74157087d1e9
mounirchebbi <2157@holbertonschool.com>
2025-02-02 14:26:15 +0100

    try return func

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 101-safe_function.py
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
