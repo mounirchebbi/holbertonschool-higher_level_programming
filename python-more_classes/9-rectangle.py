c0263f788c52214a60ce99eaf2153bc38b71c49a
mounirchebbi <2157@holbertonschool.com>
2025-02-05 14:29:58 +0100

    cls

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return(cls(size, size))
+        return cls(size, size)
 
     def area(self):
         return self.__width * self.__height
