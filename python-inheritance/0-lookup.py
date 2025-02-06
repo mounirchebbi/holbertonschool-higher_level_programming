be83030d842fc318ca30a81f425345cc244265b5
mounirchebbi <2157@holbertonschool.com>
2025-02-06 12:25:29 +0100

    dir(object)

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-lookup.py
+"""Lookup for object methods and attributes"""
+
+
+def lookup(obj):
+    """return list of objects methods and attributes """
+    return dir(obj)
