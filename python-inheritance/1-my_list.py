8a4b50880338e13bb23a13d0e748e0a54ccb8f7f
mounirchebbi <2157@holbertonschool.com>
2025-02-06 14:34:16 +0100

    inherit withount ___init__

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-my_list.py
+"""List module"""
+
+
+class Mylist (list):
+    """List clas that inherits from list"""
+
+    def print_sorted(self):
+        """method that print a sorted list"""
+        print(sorted(self))
