14e553b8c6aa764284a2e861e9cc22eab326bed7
mounirchebbi <2157@holbertonschool.com>
2025-02-06 19:14:46 +0100

    isinstance(obj, class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+"""Verify if object is instance of specific class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if obj is instance of a_class"""
+
+    return isinstance(obj, a_class)
