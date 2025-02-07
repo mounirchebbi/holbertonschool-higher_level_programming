1ca2d18dfd5f3019316e0230517e1dba18da08cb
mounirchebbi <2157@holbertonschool.com>
2025-02-07 20:26:24 +0100

    isinstance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""Module that cheks if the object is an instance of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """return true if obj is an instance of a_class"""
+    return isinstance(obj, a_class)
