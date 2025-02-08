56c04e171807cda69bbd316b99933c9cb416572a
mounirchebbi <2157@holbertonschool.com>
2025-02-08 18:35:12 +0100

    inherit method

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# 8-rectangle.py
+"""BaseGeometry module """
+
+
+class BaseGeometry:
+    """BaseGeometry class definition"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        super().integer_validator(width, height)
+        self.__width = width
+        self.__height = height
