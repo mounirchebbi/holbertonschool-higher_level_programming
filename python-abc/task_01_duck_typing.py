c7902adc33d6d47969048c424b6d968715aa602a
mounirchebbi <2157@holbertonschool.com>
2025-02-10 16:08:45 +0100

    stand alone function

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -18,10 +18,10 @@ class Shape(ABC):
 
 
 class Rectangle(Shape):
-    """subclass Rectangle"""
+    """Rectangle subclass of Shape"""
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.width = abs(width)
+        self.height = abs(height)
 
     def area(self):
         return self.width * self.height
@@ -31,9 +31,9 @@ class Rectangle(Shape):
 
 
 class Circle(Shape):
-    """subclass Circle"""
+    """Circle subclass of shape"""
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
         return (self.radius ** 2) * math.pi
@@ -42,7 +42,14 @@ class Circle(Shape):
         return self.radius * 2 * math.pi
 
 
-# stand alone function
 def shape_info(shape):
+    """stand alone function ducktyping area() perimeter()"""
     print(f"Area: {shape.area()}")
     print(f"Perimeter:{shape.perimeter()}")
+
+
+circle = Circle(radius=5)
+rectangle = Rectangle(width=4, height=7)
+
+shape_info(circle)
+shape_info(rectangle)
