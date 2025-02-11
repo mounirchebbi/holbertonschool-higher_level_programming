544eb8663a86334f4a3ac95983e651cab6b9a67d
mounirchebbi <2157@holbertonschool.com>
2025-02-11 21:44:56 +0100

    file.write

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-write_file.py
+"""Module that defines write_file function"""
+
+
+def write_file(filename="", text=""):
+    """write text to file and return number of characters written"""
+    with open(filename, 'w') as file:
+        count = 0
+        for c in text:
+            file.write(c)
+            count += 1
+
+    return count
