d2cd3ed3d969df83e6e54b925ebb68218370e1bd
mounirchebbi <2157@holbertonschool.com>
2025-02-13 21:17:38 +0100

    fixes

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,5 @@
 def read_file(filename=""):
     """function that reads a file"""
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        content = file.read()
+        print(content)
