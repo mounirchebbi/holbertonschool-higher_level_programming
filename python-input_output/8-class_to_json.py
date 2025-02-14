c27fb7a6903e95312c5edd2b26c9e83e8dbc53d3
mounirchebbi <2157@holbertonschool.com>
2025-02-14 12:03:22 +0100

    int str float bool list dict -> serializables

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,5 +4,17 @@
 
 
 def class_to_json(obj):
-    """class object to json object"""
-    return(obj.__dict__)
+    """convert class to serializable dict"""
+
+    serializable_obj = {}
+    for key, value in obj.__dict__.items():
+        # case if value is serializable
+        if isinstance(value, (int, str, float, bool, list, dict)):
+            serializable_obj[key] = value
+        # case of tuple or set convert to list
+        elif isinstance(value, (tuple, set)):
+            serializable_obj[key] = list(value)
+        # default convert to string
+        else:
+            serializable_obj[key] = str(value)
+    return serializable_obj
