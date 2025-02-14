0813985df331bb3af0f5e4fa574807223cfe3cd6
mounirchebbi <2157@holbertonschool.com>
2025-02-14 10:45:57 +0100

    fixes

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,11 +3,16 @@
 """Module that save argument into json file then loads and print them"""
 import json
 import sys
+import os
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+file_name = "add_item.json"
 args = sys.argv
 json_list = []
+if os.path.exists(file_name):
+    json_list = load_from_json_file(file_name)
 for i in range(1, len(args)):
     json_list.append(args[i])
-save_to_json_file(json_list, "add_item.json")
+save_to_json_file(json_list, file_name)
