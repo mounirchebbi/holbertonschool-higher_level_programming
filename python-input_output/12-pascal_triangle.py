cc26352cf0316e30c61430638a3705a82fcc52d1
mounirchebbi <2157@holbertonschool.com>
2025-02-14 20:01:03 +0100

    list = [1] * n

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,9 +12,21 @@ def pascal_triangle(n):
             triangle_row = [1] * (row + 1)
 
             for i in range(1, row):
-                triangle_row[i] = triangle[row - 1][i - 1]
+                triangle_row[i] = triangle[row - 1][i - 1]\
                 + triangle[row - 1][i]
 
             triangle.append(triangle_row)
 
     return triangle
+
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
