098baa5cc565f0814603c1362cf08351d543b686
mounirchebbi <2157@holbertonschool.com>
2025-02-17 16:11:47 +0100

    csv to json

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# task_02_csv.py
+"""Module to convert csv to json"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file):
+    """return true when convertion successful"""
+    try:
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            try:
+                with open("data.json", 'w') as file2:
+                    json.dump(list(reader), file2)
+            except Exception as e:
+                return False
+    except Exception as e:
+        return False
