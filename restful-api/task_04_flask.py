3e18735be33ab6f8c089da7826a20334106fed48
mounirchebbi <2157@holbertonschool.com>
2025-03-06 09:06:49 +0100

    step by step check

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -3,13 +3,22 @@
 """implementation of flask webserver"""
 
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
+
+# In-memory users dictionary
+#users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 
 # Root route
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
+# Route to users data Returns only usernames
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
 
 # Run the Flask app
 if __name__ == "__main__":
