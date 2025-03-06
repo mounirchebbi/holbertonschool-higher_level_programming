9ca33c9836b02cca9db39f81b155ec3b734bd51e
mounirchebbi <2157@holbertonschool.com>
2025-03-06 09:11:19 +0100

    step by step check

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -8,7 +8,7 @@ from flask import jsonify
 app = Flask(__name__)
 
 # In-memory users dictionary
-#users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 
 # Root route
 @app.route("/")
@@ -20,6 +20,25 @@ def home():
 def get_data():
     return jsonify(list(users.keys()))
 
+# Status endpoint
+@app.route("/status")
+def status():
+    return "OK"
+
+
+# Dynamic route for fetching user data
+@app.route("/users/<username>")
+def get_user(username):
+    if username and username in users.keys():
+        user_data = users.get(username)
+        output = {"username": username,
+                  "name": user_data["name"],
+                  "age": user_data["age"],
+                  "city": user_data["city"]}
+        return jsonify(output)
+    return jsonify({"error": "User not found"}), 404
+
+
 # Run the Flask app
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
