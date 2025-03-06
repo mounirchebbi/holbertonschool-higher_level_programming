c76894a2bcd82f4919d62b70e666e7688a40b20a
mounirchebbi <2157@holbertonschool.com>
2025-03-06 09:01:30 +0100

    step by step check

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -5,3 +5,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# Root route
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run()
