c503b5c0598181ebd6f5fed3263630e73068ccdc
mounirchebbi <2157@holbertonschool.com>
2025-03-06 10:22:45 +0100

    step by step check

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -17,51 +17,45 @@ def home():
     return "Welcome to the Flask API!"
 
 
-@app.route("/data")
+@app.route('/data')
 def get_data():
     """Route to users data Returns only usernames"""
 
     return jsonify(list(users.keys()))
 
 
-@app.route("/status")
+@app.route('/status')
 def status():
     """Status endpoint"""
 
     return "OK"
 
 
-@app.route("/users/<username>")
-def get_user(username):
-    """Dynamic route for fetching user data"""
+@app.route('/users/<username>')
+def get_user_info(username):
+    """route for fetching user data"""
 
     if username in users:
         return jsonify(users[username])
     return jsonify({"error": "User not found"}), 404
 
 
-@app.route("/add_user", methods=["POST"])
+@app.route('/add_user', methods=['POST'])
 def add_user():
     """Route add user via POST request"""
-
-    data = request.get_json()
-    if not data or "username" not in data:
+    userdata = request.get_json()
+    username = userdata.get('username')
+    if not username:
         return jsonify({"error": "Username is required"}), 400
-
-    username = data["username"]
-    if username in users:
-        return jsonify({"error": "User already exists"}), 400
-
     users[username] = {
-        "username": username,
-        "name": data["name"],
-        "age": data["age"],
-        "city": data["city"]
+        'username': username,
+        'name': userdata.get('name'),
+        'age': userdata.get('age'),
+        'city': userdata.get('city')
     }
 
     return jsonify({"message": "User added", "user": users[username]}), 201
 
 
-# Run the Flask app
 if __name__ == "__main__":
     app.run()
