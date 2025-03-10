bc626d7016f62059f0677d464d49827abda12e23
mounirchebbi <2157@holbertonschool.com>
2025-03-10 17:06:15 +0100

    create grant flush

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- create user with all priviledeges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEDGES on *.* to user_0d_1;
-FLUSH PRIVILEDGES;
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+FLUSH PRIVILEGES;
