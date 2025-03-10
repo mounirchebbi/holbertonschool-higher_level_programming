53eeeb2af4805e8f4506bc77aa3cf590c6a42de8
mounirchebbi <2157@holbertonschool.com>
2025-03-10 17:41:29 +0100

    unique not null auto_increment primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -4,6 +4,7 @@ USE hbtn_0d_usa;
 
 --create table
 CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
     );
