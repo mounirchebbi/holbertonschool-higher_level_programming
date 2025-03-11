4023043163138e1273cf794ac2b087a9c09ab72d
mounirchebbi <2157@holbertonschool.com>
2025-03-11 13:18:22 +0100

    filter response

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Module to use Mysqldb"""
+import MySQLdb
+import sys
+
+
+def List_Filtered_states(username, password, db_name):
+    """connect to db to retrives recors from states table"""
+
+    # connection to db
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+    )
+
+    # using cursor to fetch response
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    response = cursor.fetchall()
+
+    # printing response
+    for row in response:
+        if row[1][0] == 'N':
+            print(row)
+
+    # closing connexions
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <username> <password> <database>")
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    List_Filtered_states(username, password, db_name)
