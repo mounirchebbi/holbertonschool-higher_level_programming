baf66ac0d9d952116f15cab07ea11a8e2fe56a4e
mounirchebbi <2157@holbertonschool.com>
2025-03-11 13:03:28 +0100

    fixes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -30,6 +30,9 @@ def List_states(username, password, db_name):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <username> <password> <database>")
+        sys.exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
