674b790a65280c5f1edfb009f46d63a22caa779a
mounirchebbi <2157@holbertonschool.com>
2025-03-16 09:16:37 +0100

    session.querry.orderby.all.first

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Script to list first State object from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Check arguments
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <username> <password> <database>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Create engine to connect to MySQL server
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    # Create a configured "Session" class and session instance
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query first State object
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Display result
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+    # Close the session
+    session.close()
