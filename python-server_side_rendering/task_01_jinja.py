80596d74d780fda5787e395ee1b09916a531f0d9
mounirchebbi <2157@holbertonschool.com>
2025-04-19 14:12:12 +0100

    Dynamic Template

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import json
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +15,19 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    items_data = []
+    json_path = os.path.join(app.root_path, 'items.json')
+    try:
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+            items_data = data.get("items", [])
+    except Exception as e:
+        print(f"Error reading items.json: {e}")
+
+    return render_template('items.html', items=items_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
