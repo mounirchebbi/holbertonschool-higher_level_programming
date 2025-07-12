cda79a1414d3fa596581fe6c20a11bffb069918a
mounirchebbi <2157@holbertonschool.com>
2025-07-12 07:06:52 +0100

    keep track of how many times the function has been called

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -4,12 +4,3 @@ def magic_string(_count=[0]):
 
     _count[0] += 1
     return ", ".join(["BestSchool"] * _count[0])
-
-
-"""_count is a mutable default argument (a list with one element).
-
-It keeps track of how many times the function has been called.
-
-Each call increases the count
-
-and returns "BestSchool" repeated that many times, joined with ", "."""
